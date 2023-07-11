ff799b43a5ae582f89180739530d73eb5109c777
mama679 <ingmariomanotas@hotmail.com>
2023-07-10 21:43:04 -0500

    Primera tabla

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -8,12 +11,13 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-
-</style>
---
+++ src/components/TableUser.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="table-user">
+        <table class="table">
+            <thead>
+                <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in users" :key="user.id">
+                    <td>{{ user.nombre }}</td>
+                    <td>{{ user.apellido }}</td>
+                    <td>{{ user.email }}</td>
+                </tr>
+            </tbody>
+
+        </table>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'table-users',
+    props:{
+        users:Array
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
