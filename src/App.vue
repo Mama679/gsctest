6b4b8b373bd51b28015eabd6361d162656ccb039
mama679 <ingmariomanotas@hotmail.com>
2023-07-10 21:49:47 -0500

    Agregar vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,57 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Usuarios</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <TableUser :users="users"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TableUser from './components/TableUser.vue'
 
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
+    TableUser
   },
-
-  data: () => ({
-    //
-  }),
+  data(){
+    return{
+      users: [
+        {
+          id: 1,
+          nombre: 'Jon',
+          apellido: 'Nieve',
+          email: 'jon@email.com',
+        },
+        {
+          id: 2,
+          nombre: 'Tyrion',
+          apellido: 'Lannister',
+          email: 'tyrion@email.com',
+        },
+        {
+          id: 3,
+          nombre: 'Daenerys',
+          apellido: 'Targaryen',
+          email: 'daenerys@email.com',
+        },
+      ]
+    }
+  }
 }
 </script>
+
+<style scoped>
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+
+</style>
